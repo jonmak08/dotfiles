b9fa28f0051073ed1afee318c39523d676eb0d31
Jonathan Mak <860951+jonmak08@users.noreply.github.com>
2025-02-06 12:21:06 -0800

    fix(gnupg): Use .old suffix instead

--- gnupg/setup.sh
+++ gnupg/setup.sh
@@ -9,13 +9,13 @@ echo "Setting up ${GNUPG}..."
 [[ ! -d "${GNUPG_DIR}" ]] && mkdir -p "${GNUPG_DIR}"
 
 # Rename ~/.gnupg/gpg-agent.conf to /.gnupg/gpg-agent.conf.bak
-[[ -f "${GNUPG_DIR}/gpg-agent.conf" ]] && mv "${GNUPG_DIR}/gpg-agent.conf" "${GNUPG_DIR}/gpg-agent.conf.bak"
+[[ -f "${GNUPG_DIR}/gpg-agent.conf" ]] && mv "${GNUPG_DIR}/gpg-agent.conf" "${GNUPG_DIR}/gpg-agent.conf.old"
 
 # Create symlink of ~/.gnupg/gpg-agent.conf from ~/dotfiles/gnupg/gpg-agent.conf
 ln -s "${HOME}/dotfiles/${GNUPG}/gpg-agent.conf" "${GNUPG_DIR}/gpg-agent.conf"
 
 # Rename ~/.gnupg/gpg.conf to /.gnupg/gpg.conf.bak
-[[ -f "${GNUPG_DIR}/gpg.conf" ]] && mv "${GNUPG_DIR}/gpg.conf" "${GNUPG_DIR}/gpg.conf.bak"
+[[ -f "${GNUPG_DIR}/gpg.conf" ]] && mv "${GNUPG_DIR}/gpg.conf" "${GNUPG_DIR}/gpg.conf.old"
 
 # Create symlink of ~/.gnupg/gpg.conf from ~/dotfiles/gnupg/gpg.conf
 ln -s "${HOME}/dotfiles/${GNUPG}/gpg.conf" "${GNUPG_DIR}/gpg.conf"
